6aa1ea0f390db55438f808d537bd29117015e319
Peder Soeraas <soras.peder@gmail.com>
2020-07-06 08:35:10 -0400

    K-Teg

--- 2019_Programs_Ontario/Kenjgewin_Teg.R
+++ 2019_Programs_Ontario/Kenjgewin_Teg.R
@@ -6,13 +6,25 @@
 Institution <- CIConnexName("Kenjgewin")
 webPage <- read_webPage('https://www.kenjgewinteg.ca/post-secondary-programs---current.html')
 
-programs = webPage %>% 
-  html_nodes("div.wsite-multicol-table-wrap") %>% 
-  html_nodes("table") %>% 
-  html_table(fill = T) %>% 
-  bind_rows()
-
-## start here
-
+programs = tibble(Detail = webPage %>% 
+                    html_nodes("div.wsite-multicol") %>%
+                    html_nodes("td.wsite-multicol-col") %>% 
+                    html_text() %>% 
+                    str_split("\n\n\r\n\n\n[[:digit:]]\\.") %>% 
+                    unlist()) %>% 
+  separate(Detail, c("Program", "Credential"), sep =  "CREDENTIAL:") %>% 
+  filter(!is.na(Credential)) %>% 
+  separate(Credential, c("Credential", "Duration"), sep = "PROGRAM LENGTH:") %>% 
+  separate(Duration, c("Duration", "Start_date"), sep = "PROGRAM START:|START DATE:") %>% 
+  separate(Start_date, c("Start_date", "Tuition"), sep = "FEES/TUITION:") %>% 
+  separate(Tuition, c("Tuition", "Description"), sep = "start\\)|change\\)|textbooks") %>% 
+  mutate(Campus = NA,
+         url = NA,
+         WIL = NA,
+         institution_name = Institution$institution_name,
+         prov_terr = Institution$prov_terr,
+         record_id = Institution$record_id,
+         member = Institution$member)
+  
 
 save_out_standard_file(programs, Institution$institution_name, "Ontario")
